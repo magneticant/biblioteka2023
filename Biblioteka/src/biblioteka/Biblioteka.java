906279826e9ab9fdadcd0feb636b4c5f47ca451d
magneticant <milance.za019@gmail.com>
2023-03-11 09:39:28 +0100

    Dodata klasa Biblioteka koja implementira interfejs BibliotekaInterfejs

---
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -0,0 +1,45 @@
+package biblioteka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs{
+private List<Knjiga> knjige;
+
+public Biblioteka() {
+	// TODO Auto-generated constructor stub
+	knjige = new ArrayList<>();
+}
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		// TODO Auto-generated method stub
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		// TODO Auto-generated method stub
+		List<Knjiga> result = new ArrayList<>();
+		for(Knjiga k : knjige) {
+			if(k.getIsbn() == isbn)
+				result.add(k);
+		}
+		return result;
+	}
+
+}
